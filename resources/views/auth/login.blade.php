6659173ec704a43f84a206ac9ce7d9e7cb6e1026
shinopai <harbor.shinozaki@gmail.com>
2024-05-12 00:33:11 +0000

    登録画面＆ログイン画面 デザイン改修

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,19 +2,19 @@
     <!-- Session Status -->
     <x-auth-session-status class="mb-4" :status="session('status')" />
 
-    <form method="POST" action="{{ route('login') }}">
+    <form method="POST" action="{{ route('login') }}" class="auth-form" novalidate>
         @csrf
-
+        <h2 class="auth-form__heading">ログイン</h2>
         <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
+        <div class="auth-form__item">
+            <x-input-label for="email" :value="__('メールアドレス')" /><br>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="auth-form__item">
+            <x-input-label for="password" :value="__('パスワード')" /><br>
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -24,24 +24,23 @@
             <x-input-error :messages="$errors->get('password')" class="mt-2" />
         </div>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
-            </label>
+        <div class="auth-form__item">
+            <x-primary-button class="ms-3">
+                {{ __('ログイン') }}
+            </x-primary-button>
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
+        <div class="auth-form__item flex-col">
+            @if (Route::has('register'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('register') }}">
+                    {{ __('アカウントをお持ちでない方はこちら') }}
                 </a>
             @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
+                    {{ __('パスワードをお忘れの方はこちら') }}
+                </a>
+            @endif
         </div>
     </form>
 </x-guest-layout>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,24 +1,24 @@
 <x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
+    <form method="POST" action="{{ route('register') }}" class="auth-form" novalidate>
         @csrf
-
+        <h2 class="auth-form__heading">会員情報登録</h2>
         <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
+        <div class="auth-form__item">
+            <x-input-label for="name" :value="__('名前')" /><br>
             <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
             <x-input-error :messages="$errors->get('name')" class="mt-2" />
         </div>
 
         <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
+        <div class="auth-form__item">
+            <x-input-label for="email" :value="__('メールアドレス')" /><br>
             <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
         <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
+        <div class="auth-form__item">
+            <x-input-label for="password" :value="__('パスワード')" /><br>
 
             <x-text-input id="password" class="block mt-1 w-full"
                             type="password"
@@ -29,8 +29,8 @@
         </div>
 
         <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
+        <div class="auth-form__item">
+            <x-input-label for="password_confirmation" :value="__('パスワード(確認)')" /><br>
 
             <x-text-input id="password_confirmation" class="block mt-1 w-full"
                             type="password"
@@ -39,14 +39,16 @@
             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
+        <div class="auth-form__item">
             <x-primary-button class="ms-4">
-                {{ __('Register') }}
+                {{ __('会員登録') }}
             </x-primary-button>
+        </div>
+
+        <div class="auth-form__item flex-col">
+            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
+                {{ __('アカウントをお持ちの方はこちら') }}
+            </a>
         </div>
     </form>
 </x-guest-layout>
